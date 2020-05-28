bb923f98451064c38e422a6044eead3d13974262
maxgoover <maxgoover@gmail.com>
2020-05-28 10:39:38 +0300

    Moved i18n into src

---
+++ src/i18n/messages/modules/ru.js
@@ -0,0 +1,69 @@
+export default {
+  app: {
+    defaultImage: '/img/default-image.png',
+    defaultUserImage: '/img/default-user.png',
+    mainImage: '/img/main-image.jpg',
+    name: 'Scorecard'
+  },
+  auth: {
+    authError: 'Неверный пароль или логин',
+    cannotSignUp: 'На данный момент нет возможности зарегистироваться.',
+    createAccount: 'Создать аккаунт',
+    joinUs: 'Вступить в союз',
+    login: 'Войти',
+    logout: 'Выйти',
+    noAccount: 'Еще нет аккаунта?',
+    password: 'Пароль',
+    passwordRequired: 'Необходимо ввести пароль',
+    rememberMe: 'Запомнить',
+    signUp: 'Зарегистрируйтесь',
+    username: 'Логин',
+    usernameRequired: 'Необходимо ввести логин'
+  },
+  common: {
+    add: 'Добавить',
+    choosePeriod: 'Выберите период',
+    enterValue: 'Введите значение'
+  },
+  content: {
+    goBack: 'Назад',
+    saveChanges: 'Сохранить изменения',
+    search: 'Поиск'
+  },
+  error: {
+    pageNotFound: 'Страница не найдена',
+    tryFollowingPages: 'Пожалуйста, попробуйте перейти на одну из следующих страниц:'
+  },
+  filter: {
+    nameProject: 'Название проекта'
+  },
+  menu: {
+    dashboard: 'Приборная панель',
+    login: 'Авторизация',
+    main: 'Главная',
+    targetEditor: 'Редактор таргетов',
+    userActions: 'Меню пользователя'
+  },
+  target: {
+    targets: 'Таргеты'
+  },
+  targetEditor: {
+    autoUpdate: 'Автообновление',
+    inversion: 'Инверсивность'
+  },
+  typesLoginForm: {
+    aaa: 'AAA',
+    scorecard: 'Scorecard',
+    typesLoginForm: 'Тип авторизации'
+  },
+  user: {
+    avatar: 'Аватар'
+  },
+  validation: {
+    required: 'Обязательное значение'
+  }
+}
+// зеленая - создает
+// голубая - редактирует
+// желтая - предупреждать
+// красная - удалять (или ошибка)
