279fb78c14b96760646fdc36ee05f7cb5617047a
maxgoover <maxgoover@gmail.com>
2020-05-28 18:36:15 +0300

    Created page "Login"

--- src/App.vue
+++ src/App.vue
@@ -38,23 +38,13 @@
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+<!--      <HelloWorld/>-->
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
-
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld
-  },
-
-  data: () => ({
-    //
-  })
+  name: 'App'
 }
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -17,6 +17,14 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "login" */ '../components/authorization/Login.vue')
   }
 ]
 
