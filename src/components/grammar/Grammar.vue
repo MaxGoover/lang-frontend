78ad0f8aab31e3596f1eac4b9f5174313ba006a4
maxgoover <maxgoover@gmail.com>
2020-06-03 10:11:39 +0300

    Created page Grammar

---
+++ src/components/grammar/Grammar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <!--Верхнее меню-->
+        <toolbar/>
+    </div>
+</template>
+
+<script>
+const Toolbar = () => import('../../components/toolbar/Toolbar')
+
+export default {
+  name: 'Grammar',
+  components: { Toolbar }
+}
+</script>
+
+<style scoped>
+
+</style>
