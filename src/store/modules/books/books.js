f497cb9ec7c6cca720c5c5f9962b9d9b1ba85396
maxgoover <maxgoover@gmail.com>
2020-06-14 18:27:30 +0300

    Added text bookParts

--- src/store/modules/books/books.js
+++ src/store/modules/books/books.js
@@ -98,6 +98,75 @@ export default {
           }
         ]
       }
+    ],
+    bookParts: [
+      {
+        bookId: 'aafw7efw34wA2e2wG',
+        bookPartId: 'Wf783hfew',
+        bookTitle: 'Harry Potter and the Philosopher\'s stone - 3',
+        partTitle: 'Kapitel 1',
+        youtubeId: 'aiwu5w26x26d',
+        content: [
+          {
+            sentences: [
+              {
+                origText: 'Origin text part 1',
+                transText: 'Оригинальный текст, часть 1'
+              },
+              {
+                origText: 'Origin text part 2',
+                transText: 'Оригинальный текст, часть 2'
+              },
+              {
+                origText: 'Origin text part 3',
+                transText: 'Оригинальный текст, часть 3'
+              }
+            ]
+          },
+          {
+            sentences: [
+              {
+                origText: 'Origin text part 4',
+                transText: 'Оригинальный текст, часть 4'
+              },
+              {
+                origText: 'Origin text part 5',
+                transText: 'Оригинальный текст, часть 5'
+              },
+              {
+                origText: 'Origin text part 6',
+                transText: 'Оригинальный текст, часть 6'
+              },
+              {
+                origText: 'Origin text part 7',
+                transText: 'Оригинальный текст, часть 7'
+              }
+            ]
+          }
+        ],
+        words: [
+          {
+            origWord: 'Hello1',
+            transWord: 'Привет1'
+          },
+          {
+            origWord: 'Hello2',
+            transWord: 'Привет2'
+          },
+          {
+            origWord: 'Hello3',
+            transWord: 'Привет3'
+          },
+          {
+            origWord: 'Hello4',
+            transWord: 'Привет4'
+          },
+          {
+            origWord: 'Hello5',
+            transWord: 'Привет5'
+          },
+        ]
+      }
     ]
   },
   getters: {
