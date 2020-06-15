fdfa6834501692d42b15567d1b1c9238cbe637c9
maxgoover <maxgoover@gmail.com>
2020-06-15 10:56:51 +0300

    Added bookPartcontent

--- src/components/books/BookPart.vue
+++ src/components/books/BookPart.vue
@@ -2,15 +2,23 @@
   <v-container grid-list-md v-if="bookPart">
     <v-layout row wrap>
       <v-flex xs12 sm10 offset-sm1>
-          <!--Контент-->
+        <book-part-content :part="bookPart" />
+      </v-flex>
+      <v-flex xs12 sm10 offset-sm1>
+<!--        <book-part-words :words="bookPart.words" />-->
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+const BookPartContent = () => import(/* webpackChunkName: "bookPartContent" */ './BookPartContent')
+
 export default {
   name: 'BookPart',
+  components: {
+    BookPartContent
+  },
   props: {
     bookId: { type: String },
     partId: { type: String }
---
+++ src/components/books/BookPartContent.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card class="pa-2">
+
+    <!--Заголовок-->
+    <div class="display-1 text-center">{{ part.bookTitle }}</div>
+    <div class="headline">{{ part.partTitle }}</div>
+
+    <!--Youtube-->
+    <div class="text-center mt-2 mb-2">
+      <!--Youtube плеер-->
+      <div class="headline" :width="width">Здесь плеер Youtube</div>
+    </div>
+  </v-card>
+</template>
+
+<script>
+import CommonHelper from '../../helpers/CommonHelper'
+
+export default {
+  name: 'BookPartContent',
+  props: {
+    part: { type: Object }
+  },
+  computed: {
+    width () {
+      return CommonHelper.widthBreakpoints(this.$vuetify.breakpoint.name)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
